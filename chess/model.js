5f822b331c5aea8a9f7556c6b8f66d89b68bd18d
michaeldias-code <michaelrpdias@gmail.com>
2025-12-03 18:03:16 -0300

    Update model.js

--- chess/model.js
+++ chess/model.js
@@ -1,5 +1,5 @@
 // model.js
-class ModelChess {
+class ChessModel {
     constructor() {
         this.board = this.createBoard();
         this.turn = 'brancas';
@@ -57,6 +57,7 @@ class ModelChess {
         if (!piece || piece.cor !== this.turn) return [];
 
         const moves = [];
+
         const addMove = (to) => {
             if (to < 0 || to >= 64) return;
             const target = this.board[to];
@@ -65,118 +66,87 @@ class ModelChess {
 
         switch(piece.tipo) {
             case '♙':
-                if (pos - 8 >= 0 && !this.board[pos-8]) moves.push(pos-8);
-                if (Math.floor(pos/8) === 6 && !this.board[pos-16]) moves.push(pos-16);
-                if (pos%8 !== 0 && this.board[pos-9] && this.board[pos-9].cor === 'pretas') moves.push(pos-9);
-                if (pos%8 !== 7 && this.board[pos-7] && this.board[pos-7].cor === 'pretas') moves.push(pos-7);
+                if (this.board[pos-8] === null) moves.push(pos-8);
+                if (Math.floor(pos/8) === 6 && this.board[pos-16] === null) moves.push(pos-16);
+                if (this.board[pos-9] && this.board[pos-9].cor === 'pretas' && pos%8 !== 0) moves.push(pos-9);
+                if (this.board[pos-7] && this.board[pos-7].cor === 'pretas' && pos%8 !== 7) moves.push(pos-7);
                 break;
             case '♟':
-                if (pos + 8 < 64 && !this.board[pos+8]) moves.push(pos+8);
-                if (Math.floor(pos/8) === 1 && !this.board[pos+16]) moves.push(pos+16);
-                if (pos%8 !== 7 && this.board[pos+9] && this.board[pos+9].cor === 'brancas') moves.push(pos+9);
-                if (pos%8 !== 0 && this.board[pos+7] && this.board[pos+7].cor === 'brancas') moves.push(pos+7);
+                if (this.board[pos+8] === null) moves.push(pos+8);
+                if (Math.floor(pos/8) === 1 && this.board[pos+16] === null) moves.push(pos+16);
+                if (this.board[pos+9] && this.board[pos+9].cor === 'brancas' && pos%8 !== 7) moves.push(pos+9);
+                if (this.board[pos+7] && this.board[pos+7].cor === 'brancas' && pos%8 !== 0) moves.push(pos+7);
                 break;
-            case '♖': case '♜':
-                moves.push(...this.getLinearMoves(pos, [-1,1,-8,8]));
+            case '♖':
+            case '♜':
+                const directions = [-1, 1, -8, 8];
+                for (let d of directions) {
+                    let p = pos + d;
+                    while (p >=0 && p < 64 && (Math.floor(p/8) === Math.floor(pos/8) || d % 8 === 0)) {
+                        if (!this.board[p]) moves.push(p);
+                        else {
+                            if (this.board[p].cor !== piece.cor) moves.push(p);
+                            break;
+                        }
+                        p += d;
+                    }
+                }
                 break;
-            case '♘': case '♞':
-                moves.push(...this.getKnightMoves(pos));
+            case '♘':
+            case '♞':
+                const knightMoves = [-17,-15,-10,-6,6,10,15,17];
+                for (let m of knightMoves) addMove(pos+m);
                 break;
-            case '♗': case '♝':
-                moves.push(...this.getDiagonalMoves(pos, [-9,-7,7,9]));
+            case '♗':
+            case '♝':
+                const diag = [-9,-7,7,9];
+                for (let d of diag) {
+                    let p = pos+d;
+                    while (p>=0 && p<64 && Math.abs((p%8)-(pos%8)) === Math.abs(d%8)) {
+                        if (!this.board[p]) moves.push(p);
+                        else {
+                            if (this.board[p].cor !== piece.cor) moves.push(p);
+                            break;
+                        }
+                        p += d;
+                    }
+                }
                 break;
-            case '♕': case '♛':
+            case '♕':
+            case '♛':
                 moves.push(...this.getQueenMoves(pos));
                 break;
-            case '♔': case '♚':
-                moves.push(...this.getKingMoves(pos));
+            case '♔':
+            case '♚':
+                const kingMoves = [-9,-8,-7,-1,1,7,8,9];
+                for (let k of kingMoves) addMove(pos+k);
                 break;
-        }
-
-        return moves;
-    }
-
-    getLinearMoves(pos, dirs) {
-        const moves = [];
-        const piece = this.board[pos];
-        for (let d of dirs) {
-            let p = pos + d;
-            while (p >= 0 && p < 64 && this.isSameLine(pos,p,d)) {
-                if (!this.board[p]) moves.push(p);
-                else {
-                    if (this.board[p].cor !== piece.cor) moves.push(p);
-                    break;
-                }
-                p += d;
-            }
-        }
-        return moves;
-    }
-
-    getDiagonalMoves(pos, dirs) {
-        const moves = [];
-        const piece = this.board[pos];
-        for (let d of dirs) {
-            let p = pos + d;
-            while (p >= 0 && p < 64 && this.isDiagonal(pos,p,d)) {
-                if (!this.board[p]) moves.push(p);
-                else {
-                    if (this.board[p].cor !== piece.cor) moves.push(p);
-                    break;
-                }
-                p += d;
-            }
-        }
-        return moves;
-    }
-
-    getKnightMoves(pos) {
-        const moves = [];
-        const piece = this.board[pos];
-        const offsets = [-17,-15,-10,-6,6,10,15,17];
-        for (let o of offsets) {
-            const t = pos+o;
-            if (t<0||t>63) continue;
-            const colDiff = Math.abs((pos%8)-(t%8));
-            if (colDiff>2) continue;
-            if (!this.board[t] || this.board[t].cor !== piece.cor) moves.push(t);
         }
         return moves;
     }
 
     getQueenMoves(pos) {
-        return [...this.getLinearMoves(pos,[-1,1,-8,8]), ...this.getDiagonalMoves(pos,[-9,-7,7,9])];
-    }
-
-    getKingMoves(pos) {
         const moves = [];
-        const piece = this.board[pos];
-        const offsets = [-9,-8,-7,-1,1,7,8,9];
-        for (let o of offsets) {
-            const t = pos+o;
-            if (t<0||t>63) continue;
-            const colDiff = Math.abs((pos%8)-(t%8));
-            if (colDiff>1) continue;
-            if (!this.board[t] || this.board[t].cor !== piece.cor) moves.push(t);
+        const directions = [-1,1,-8,8,-9,-7,7,9];
+        for (let d of directions) {
+            let p = pos+d;
+            while (p>=0 && p<64 && Math.abs((p%8)-(pos%8)) <=1) {
+                if (!this.board[p]) moves.push(p);
+                else {
+                    if (this.board[p].cor !== this.board[pos].cor) moves.push(p);
+                    break;
+                }
+                p += d;
+            }
         }
         return moves;
-    }
-
-    isSameLine(from,to,d) {
-        if (d === -1 || d === 1) return Math.floor(from/8) === Math.floor(to/8);
-        return true;
-    }
-
-    isDiagonal(from,to,d) {
-        const colDiff = Math.abs((from%8)-(to%8));
-        return colDiff === Math.abs(d%8);
     }
 
     isKingInCheck(color) {
         const kingPos = this.board.findIndex(p => p && p.tipo.toLowerCase() === '♔' && p.cor === color);
         for (let i=0;i<64;i++){
             const p = this.board[i];
-            if (p && p.cor !== color){
+            if (p && p.cor !== color) {
                 const moves = this.getPossibleMoves(i);
                 if (moves.includes(kingPos)) return true;
             }
@@ -205,12 +175,21 @@ class ModelChess {
     }
 
     undoMove() {
-        if (!this.history.length) return;
+        if (this.history.length === 0) return;
         const last = this.history.pop();
         this.board[last.from] = last.piece;
         this.board[last.to] = last.captured;
         this.turn = this.turn === 'brancas' ? 'pretas' : 'brancas';
     }
+
+    // método adicionado para o controller
+    getState() {
+        return {
+            board: this.board,
+            turn: this.turn,
+            history: this.history
+        };
+    }
 }
 
-console.log("ModelChess carregado com sucesso!");
+console.log("ChessModel carregado com sucesso!");
