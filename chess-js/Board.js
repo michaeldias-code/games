f3173ae4c514f75322807d7cb11da934276573b5
michaeldias-code <michaelrpdias@gmail.com>
2025-12-04 14:21:45 -0300

    Update Board.js

--- chess-js/Board.js
+++ chess-js/Board.js
@@ -1,77 +1,44 @@
 // Board.js
-import { Classes } from './config.js';
-import { Piece } from './Piece.js';
-import { MoveValidator } from './MoveValidator.js';
-
 export class Board {
     constructor() {
-        this.squares = Array(64).fill(null); // Tabuleiro 8x8
-        this.turn = 'brancas';
-        this.history = [];
-        this.validator = new MoveValidator(this.squares);
-        this.initBoard();
+        console.log("Board construído!");
+
+        // 64 casas
+        this.board = Array(64).fill(null);
+        this.setupPieces();
     }
 
-    initBoard() {
-        // Peões
-        for (let i = 8; i < 16; i++) this.squares[i] = new Piece('♟', 'pretas');
-        for (let i = 48; i < 56; i++) this.squares[i] = new Piece('♙', 'brancas');
+    setupPieces() {
+        console.log("Board: posicionando peças...");
+
+        // Peões pretos
+        for (let i = 8; i < 16; i++)
+            this.board[i] = { tipo: "♟", cor: "pretas" };
+
+        // Peões brancos
+        for (let i = 48; i < 56; i++)
+            this.board[i] = { tipo: "♙", cor: "brancas" };
 
         // Torres
-        this.squares[0] = this.squares[7] = new Piece('♜', 'pretas');
-        this.squares[56] = this.squares[63] = new Piece('♖', 'brancas');
+        this.board[0] = this.board[7] = { tipo: "♜", cor: "pretas" };
+        this.board[56] = this.board[63] = { tipo: "♖", cor: "brancas" };
 
         // Cavalos
-        this.squares[1] = this.squares[6] = new Piece('♞', 'pretas');
-        this.squares[57] = this.squares[62] = new Piece('♘', 'brancas');
+        this.board[1] = this.board[6] = { tipo: "♞", cor: "pretas" };
+        this.board[57] = this.board[62] = { tipo: "♘", cor: "brancas" };
 
         // Bispos
-        this.squares[2] = this.squares[5] = new Piece('♝', 'pretas');
-        this.squares[58] = this.squares[61] = new Piece('♗', 'brancas');
+        this.board[2] = this.board[5] = { tipo: "♝", cor: "pretas" };
+        this.board[58] = this.board[61] = { tipo: "♗", cor: "brancas" };
 
         // Rainhas
-        this.squares[3] = new Piece('♛', 'pretas');
-        this.squares[59] = new Piece('♕', 'brancas');
+        this.board[3] = { tipo: "♛", cor: "pretas" };
+        this.board[59] = { tipo: "♕", cor: "brancas" };
 
         // Reis
-        this.squares[4] = new Piece('♚', 'pretas');
-        this.squares[60] = new Piece('♔', 'brancas');
-    }
+        this.board[4] = { tipo: "♚", cor: "pretas" };
+        this.board[60] = { tipo: "♔", cor: "brancas" };
 
-    movePiece(from, to) {
-        const piece = this.squares[from];
-        if (!piece) return false;
-
-        const possibleMoves = this.validator.getPossibleMoves(from);
-        if (!possibleMoves.includes(to)) return false;
-
-        // Salva histórico
-        this.history.push({ from, to, piece, captured: this.squares[to] });
-
-        // Move
-        this.squares[to] = piece;
-        this.squares[from] = null;
-
-        // Troca turno
-        this.turn = this.turn === 'brancas' ? 'pretas' : 'brancas';
-
-        return true;
-    }
-
-    undoMove() {
-        if (!this.history.length) return;
-        const last = this.history.pop();
-        this.squares[last.from] = last.piece;
-        this.squares[last.to] = last.captured;
-        this.turn = this.turn === 'brancas' ? 'pretas' : 'brancas';
-    }
-
-    getState() {
-        return {
-            squares: this.squares,
-            turn: this.turn
-        };
+        console.log("Board pronto!");
     }
 }
-
-console.log('Board carregado!');
