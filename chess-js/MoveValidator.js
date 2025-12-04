18d5579f2165693ee039da2caca58050c6f84269
michaeldias-code <michaelrpdias@gmail.com>
2025-12-04 13:35:25 -0300

    Update MoveValidator.js

--- chess-js/MoveValidator.js
+++ chess-js/MoveValidator.js
@@ -3,8 +3,9 @@ import { Modules } from './config.js';
 
 export class MoveValidator {
   static isValidMove(board, from, to) {
-    console.log([${Modules.MoveValidator}] Validando movimento de ${from} para ${to});
+    //console.log([${Modules.MoveValidator}] Validando movimento de ${from} para ${to});
     // Aqui poderia colocar regras completas de xadrez
     return true; // placeholder
   }
 }
+
