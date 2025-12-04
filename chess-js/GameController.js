edee230f336b1a2a9e599d3f1bf2242c485cf05f
michaeldias-code <michaelrpdias@gmail.com>
2025-12-04 14:22:09 -0300

    Update GameController.js

--- chess-js/GameController.js
+++ chess-js/GameController.js
@@ -1,20 +1,24 @@
 // GameController.js
 import { Board } from './Board.js';
+import { MoveValidator } from './MoveValidator.js';
 import { AI } from './AI.js';
 import { View } from './View.js';
 
 export class GameController {
     constructor() {
-        console.log('GameController inicializando...');
-        this.board = new Board();
-        this.ai = new AI(this.board);
+        console.log("GameController inicializando...");
+
+        this.board = new Board();          // AQUI!!!
+        this.validator = new MoveValidator(this.board);
+        this.ai = new AI(this.board, this.validator);
+
         this.view = new View(this.board, this.ai, this);
-        console.log('GameController carregado!');
+
+        console.log("GameController carregado!");
     }
 
     movePiece(from, to) {
-        const success = this.board.movePiece(from, to);
-        if (success) this.view.render();
-        return success;
+        this.validator.movePiece(from, to);
+        this.view.render();
     }
 }
