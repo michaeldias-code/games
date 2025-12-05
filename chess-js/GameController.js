0e52628c0a517a4b4673f2beee2c3ca263c51bf4
michaeldias-code <michaelrpdias@gmail.com>
2025-12-04 21:49:40 -0300

    Update GameController.js

--- chess-js/GameController.js
+++ chess-js/GameController.js
@@ -1,4 +1,3 @@
-// GameController.js
 import { Board } from './Board.js';
 import { MoveValidator } from './MoveValidator.js';
 import { AI } from './AI.js';
@@ -17,27 +16,30 @@ export class GameController {
         // IA trabalha com board e validator
         this.ai = new AI(this.board, this.validator);
 
-        // View recebe board, ai e controller
-        this.view = new View(this.board, this.ai, this);
+        // View recebe board e controller
+        this.view = new View(this.board, this);
 
         console.log("GameController carregado!");
     }
 
+    // Função chamada pelo View quando o jogador tenta mover
     movePiece(from, to) {
         const possibleMoves = this.validator.getPossibleMoves(from);
+
         if (!possibleMoves.includes(to)) {
             console.log("Movimento inválido:", from, "->", to);
             return false;
         }
 
-        // Executa movimento
+        // Executa o movimento
         const piece = this.board.board[from];
         this.board.board[to] = piece;
         this.board.board[from] = null;
 
+        // Atualiza a interface
         this.view.render();
 
-        // IA move após jogador
+        // Depois do jogador, IA joga
         setTimeout(() => {
             this.ai.makeMove('pretas');
             this.view.render();
